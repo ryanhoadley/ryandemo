e2e65cfca73ed2cc7e6f0127729af8ec4345a907
Ryan Hoadley <Ryan.Hoadley@ad.carvana.com>
2025-01-28 09:43:23 -0700

    updated tests

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -10,4 +10,12 @@ describe("super fun time tests", () => {
   it("result tile is displayed", () => {
     cy.getByQa("result-tile").should("be.visible");
   });
+
+  it("can freetext search for a model", () => {
+    cy.get("#merch-search-bar-input").type("corvette{enter}");
+    cy.getByQa("result-tile").first().should("contain", "Corvette");
+    cy.getByQa("result-tile").last().should("contain", "Corvette");
+  });
 });
+
+// cy.get('="sc-H3mnv Jf91f"]'.click();
